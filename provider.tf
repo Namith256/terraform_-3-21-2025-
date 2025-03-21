2c60afe62135d56c14ab90f055d3a47df99c91bd
Namith256 <namith@example.com>
2025-03-21 12:46:52 +0530

    updated version 1

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "eu-north-1"
-  shared_config_files      = [~/.aws/config]
-  shared_credentials_files = [~/.aws/credentials]
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "dev"
 }
